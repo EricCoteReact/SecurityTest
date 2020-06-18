57936b42b87eaa85854a19c8393604ed5bd82416
Eric Cote <ericcote@reactacademy.ca>
2020-06-18 09:09:46 -0400

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,48 @@
+﻿using WebApplication5.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace WebApplication5.Data
+{
+
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+
+
+        //private readonly IOptions<OperationalStoreOptions> _operationalStoreOptions;
+
+
+
+        //public DbSet<PersistedGrant> PersistedGrants { get; set; }
+
+        //public DbSet<DeviceFlowCodes> DeviceFlowCodes { get; set; }
+
+        //Task<int> IPersistedGrantDbContext.SaveChangesAsync() => base.SaveChangesAsync();
+
+        //protected override void OnModelCreating(ModelBuilder builder)
+        //{
+        //    base.OnModelCreating(builder);
+        //    builder.ConfigurePersistedGrantContext(_operationalStoreOptions.Value);
+        //}
+    }
+
+
+
+
+
+    //public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    //{
+    //    public ApplicationDbContext(
+    //        DbContextOptions options,
+    //        IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+    //    {
+    //    }
+    //}
+}
---
+++ Data/Migrations/20200529133747_ajouter_departement.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication5.Data.Migrations
+{
+    public partial class ajouter_departement : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Department",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Department",
+                table: "AspNetUsers");
+        }
+    }
+}
